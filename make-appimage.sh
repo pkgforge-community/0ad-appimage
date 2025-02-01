pkgforge-community/0ad-appimage

commit bd8ebfc3a9e8d59c3c98883b11fa31cc7e89b94a

Remove unneeded trailing slashes from apt install

make-appimage.sh
@@ -46,37 +46,32 @@ fi
 
 cd "$WORKSPACE"
 
-sudo DEBIAN_FRONTEND=noninteractive -i sh -c "apt update && apt -y upgrade && \
-    apt install -y  \
-    cargo   \
-    cmake \
-    curl \
-    $CC \
-    desktop-file-utils \
-    libboost-dev    \
-    libboost-filesystem-dev \
-    libboost-system-dev   \
-    libcurl4-gnutls-dev \
-    libenet-dev \
-    libfmt-dev   \
-    libfreetype6-dev    \
-    libgloox-dev    \
-    libicu-dev \
-    libminiupnpc-dev \
-    libogg-dev  \
-    libopenal-dev   \
-    libpng-dev  \
-    libsdl2-dev \
-    libsodium-dev   \
-    libvorbis-dev \
-    libwxgtk3.0-gtk3-dev \
-    libxml2-dev \
-    llvm \
-    m4 \
-    patchelf \
-    python3 \
-    rustc   \
-    zlib1g-dev"
+sudo DEBIAN_FRONTEND=noninteractive -i sh -c "apt update && apt -y upgrade &&
+    libboost-dev
+    libboost-filesystem-dev
+    libboost-system-dev
+    libcurl4-gnutls-dev
+    libenet-dev
+    libfmt-dev
+    libfreetype6-dev
+    libgloox-dev
+    libicu-dev
+    libminiupnpc-dev
+    libogg-dev
+    libopenal-dev
+    libpng-dev
+    libsdl2-dev
+    libsodium-dev
+    libvorbis-dev
+    libwxgtk3.0-gtk3-dev
+    libxml2-dev
+    llvm
+    m4
+    patchelf
+    python3
+    rustc
+    zlib1g-dev
+"
 
 PREMAKE="premake-5.0.0-beta4-linux.tar.gz"
 if [ ! -f "$PREMAKE" ]; then

commit 85f969967279d470041d9df454ef8bd8a6f8b726

Fix apt install section (broke by bd8ebfc)

make-appimage.sh
@@ -47,6 +47,7 @@ fi
 cd "$WORKSPACE"
 
 sudo DEBIAN_FRONTEND=noninteractive -i sh -c "apt update && apt -y upgrade &&
+  apt install -y
     libboost-dev
     libboost-filesystem-dev
     libboost-system-dev